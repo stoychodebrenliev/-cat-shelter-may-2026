1b67516b96903082d38ce2b297e250bb8c6b4088
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-18 16:33:22 +0200

    add http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    res.write('Hello, World!')
+    res.end();
+})
+
+server.listen(5000, () => console.log('Server is listening on port http://localhost:5000...'));
+
+
