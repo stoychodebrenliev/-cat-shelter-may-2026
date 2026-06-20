56604836c150a394bf8b172523f40800d28d9203
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-20 12:43:50 +0200

    Add breed and add cat pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import http from "http";
 import fs from 'fs/promises';
-import path from "path";
 
-const server = http.createServer(async(req, res) => {
-    const homePage = await fs.readFile('./src/views/home/index.html', 'utf-8');
+
+const server = http.createServer(async (req, res) => {
     
+    if (req.url === '/styles/site.css') {
+        const cssContent = await fs.readFile('./src/styles/site.css', 'utf-8');
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(homePage);
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(cssContent);
 
+        return res.end();
+    }
+
+    let htmlContent = '';
+
+    if (req.url === '/') {
+        htmlContent = await fs.readFile('./src/views/home/index.html', 'utf-8');
+    } else if (req.url === '/cats/add-breed') {
+        htmlContent = await fs.readFile('./src/views/addBreed.html', 'utf-8');
+    } else if (req.url === '/cats/add-cat') {
+        htmlContent = await fs.readFile('./src/views/addCat.html', 'utf-8');
+    }
+
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(htmlContent);
     res.end();
-})
+});
 
 server.listen(5000, () => console.log('Server is listening on port http://localhost:5000...'));
 
