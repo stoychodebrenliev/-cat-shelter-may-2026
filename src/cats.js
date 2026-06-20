0c9656e94d13980e33a457a4e8bd56f0d000d445
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-20 13:11:48 +0200

    show cats dynamically based on array

---
+++ src/cats.js
@@ -0,0 +1,40 @@
+const cats = [
+    {
+        id: 1,
+        name: 'Navcho',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+        imageUrl: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg'
+    },
+    {
+        id: 2,
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg'
+    },
+    {
+        id: 3,
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+        imageUrl: 'https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg'
+    },
+    {
+        id: 4,
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        price: 350,
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+        imageUrl: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
+    },
+    {
+        id: 5,
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+        imageUrl: 'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg'
+    }
+];
+
+export default cats;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import http from "http";
 import fs from 'fs/promises';
-
+import cats from './cats.js';
 
 const server = http.createServer(async (req, res) => {
     
@@ -16,7 +16,7 @@ const server = http.createServer(async (req, res) => {
     let htmlContent = '';
 
     if (req.url === '/') {
-        htmlContent = await fs.readFile('./src/views/home/index.html', 'utf-8');
+        htmlContent = await renderHomePage();
     } else if (req.url === '/cats/add-breed') {
         htmlContent = await fs.readFile('./src/views/addBreed.html', 'utf-8');
     } else if (req.url === '/cats/add-cat') {
@@ -32,3 +32,24 @@ const server = http.createServer(async (req, res) => {
 server.listen(5000, () => console.log('Server is listening on port http://localhost:5000...'));
 
 
+async function renderHomePage() {
+    let htmlContent =await fs.readFile('./src/views/home/index.html', 'utf-8');
+    
+    const catTemplate =(cat) => `
+        <li>
+         <img src="${cat.imageUrl}" alt="${cat.name}">
+          <h3>${cat.name}</h3>
+          <p><span>Breed: </span>${cat.breed}</p>
+          <p><span>Description: </span>${cat.description}</p>
+          <ul class="buttons">
+              <li class="btn edit"><a href="">Change Info</a></li>
+              <li class="btn delete"><a href="">New Home</a></li>
+          </ul>
+        </li> `;
+
+        const catsContent = `<ul>${cats.map(cat => catTemplate(cat)).join('\n')}</ul>`;
+
+        const result = htmlContent.replace('{{cats}}', catsContent);
+    
+        return result;
+}
